133fc0164644fd63b5a25f3ff3d1c2fbf803855e
voquangtrieu1604 <19130247@st.hcmuaf.edu.vn>
2022-08-15 18:30:03 +0700

    MD5 password

--- src/main/java/edu/hcmuaf/edu/fit/project_ltw/Register.java
+++ src/main/java/edu/hcmuaf/edu/fit/project_ltw/Register.java
@@ -1,27 +1,34 @@
 package edu.hcmuaf.edu.fit.project_ltw;
 
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 @WebServlet(name = "Register", value = "/Register")
 public class Register extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("register.jsp").forward(request,response);
+        request.getRequestDispatcher("register.jsp").forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
+
+
         String name = request.getParameter("username");
         String email = request.getParameter("email");
         String password = request.getParameter("password");
 
-        edu.hcmuaf.edu.fit.project_ltw.beans.Register.registerAutoID_user(name, email, password);
+        String pass = edu.hcmuaf.edu.fit.project_ltw.beans.Register.getMD5(password);
+        edu.hcmuaf.edu.fit.project_ltw.beans.Register.registerAutoID_user(name, email, pass);
 
         String line = "<script>window.setTimeout(function(){window.location.href = \"login\";}, 1000);</script>";
         response.getWriter().println();
@@ -32,8 +39,6 @@ public class Register extends HttpServlet {
         out.println("<title>notice</title></head>");
         out.println("<body>");
         out.println(line);
-        // Echo client's request information
-        // Generate a random number upon each request
         out.println("</body>");
         out.println("</html>");
 
